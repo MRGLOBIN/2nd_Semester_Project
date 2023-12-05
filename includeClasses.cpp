703eed46618bef315dab930fe4149138f6314664
Muhammad Asim <asimqwe2@gmail.com>
2023-12-05 22:24:35 +0500

    classes code for subject lists and input file stream

--- includeClasses.cpp
+++ includeClasses.cpp
@@ -28,3 +28,58 @@ public:
     bool checkchange();
     void display();
 };
+
+struct subjectName
+{
+    string departmentName;
+    int sections;
+    CourseSubject course;
+    subjectName *next;
+};
+
+class ClassNameData
+{
+protected:
+    int values;
+    subjectName *first,
+        *newClass,
+        *restorePtr;
+};
+
+class ClassName : public ClassNameData
+{
+
+public:
+    ClassName()
+    {
+        first = NULL;
+        values = 0;
+    }
+    void addClass(string, int);
+    void addSubject(string, int);
+    void nextClass();
+    void setRestore();
+    void setValues();
+    int getValues();
+    void display();
+    friend void generateBlockClass(ClassName &, string (*)[5][8], int (*)[5][8]);
+};
+
+/* classes for file stream handlers */
+
+class InputFileStream
+{
+private:
+    char opinion;
+
+public:
+    // constructors
+    InputFileStream()
+    {
+        opinion = '\n';
+    }
+
+    // functions headers
+    void addDepartment();
+    void addCourse();
+};
