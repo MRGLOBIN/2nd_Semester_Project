5bbc462587312b7676373cf63f9b16377f08519e
Muhammad Asim <asimqwe2@gmail.com>
2023-12-07 12:12:40 +0500

    functions for className

--- functions.cpp
+++ functions.cpp
@@ -49,3 +49,59 @@ void CourseSubject::display()
         current = current->next;
     }
 }
+
+void ClassName::addClass(string name, int section)
+{
+    // creating new item for list
+    newClass = new subjectName;
+    newClass->departmentName = name;
+    newClass->sections = section;
+}
+
+void ClassName::addSubject(string name, int hours)
+{
+    // adding course to current department
+    newClass->course.addSubject(name, hours);
+}
+
+void ClassName::nextClass()
+{
+    // pointing first to this current list item
+    newClass->next = first;
+    first = newClass;
+}
+
+void ClassName::setRestore()
+{
+    restorePtr = first;
+}
+
+void ClassName::setValues()
+{
+    subjectName *current = first;
+    while (current != NULL)
+    {
+        current = current->next;
+        values++;
+    }
+}
+
+int ClassName::getValues()
+{
+    return values;
+}
+
+void ClassName::display()
+{
+    subjectName *current = first;
+    while (current != NULL)
+    {
+        cout << endl
+             << "DEPARTMENT: " << current->departmentName << endl
+             << "Section: " << current->sections << endl;
+        cout << "Current Courses are:\n";
+        current->course.display();
+        current = current->next;
+    }
+    cout << "Total number of rooms: " << totalRooms << endl;
+}
