c908f47b8d05bc00655203f568aa199b9708bf0a
Muhammad Asim <asimqwe2@gmail.com>
2023-12-09 22:57:20 +0500

    function code completed

--- functions.cpp
+++ functions.cpp
@@ -150,3 +150,165 @@ void InputFileStream::addCourse()
     fout << storageEndCourseCharacter;
     fout.close();
 }
+
+// buffer function
+
+void clearBuffer(char *buffer)
+{
+    for (int b = 0; b < 25; b++)
+    {
+        buffer[b] = '\0';
+    }
+}
+
+void readInputFromStorage(ClassName &Faculity)
+{
+    // Reading from file
+    ifstream fin("./data.txt");
+
+    // ClassName Faculity;
+    int number;
+    char data = '\0';
+    char buffer[25] = {'\0'};
+
+    fin >> data;
+
+    while (data != storageEndFileCharacter)
+    {
+        int i;
+        if (data == storageStartDepartment)
+        {
+            fin >> data;
+            for (i = 0; data != storageEndDepartment; i++)
+            {
+                buffer[i] = data;
+                fin >> data;
+
+                if (data == '%')
+                {
+                    fin >> data;
+                    number = data - 48;
+
+                    // skipping that number character
+                    fin >> data;
+                }
+            }
+            Faculity.addClass(buffer, number);
+
+            // checking for number
+            fin >> data;
+
+            // making buffer ready for next input
+            clearBuffer(buffer);
+        }
+
+        // now adding subjects for current department
+        while (data != storageEndCourseCharacter && data != storageEndFileCharacter && data != storageStartDepartment)
+        {
+            for (i = 0; data != storageEndSubjectCharacter && data != storageEndCourseCharacter; i++)
+            {
+                buffer[i] = data;
+                fin >> data;
+                // optimize required
+                if (data == '%')
+                {
+                    fin >> data;
+                    number = data - 48;
+
+                    // skipping that number character
+                    fin >> data;
+                }
+            }
+            Faculity.addSubject(buffer, number);
+
+            // making buffer ready for next input
+            clearBuffer(buffer);
+
+            fin >> data;
+        }
+        Faculity.nextClass();
+    }
+    fin >> totalRooms;
+    fin.close();
+}
+
+void generateBlockClass(ClassName &Facuilty, string (*rooms)[5][8], int (*sections)[5][8])
+{
+    //      && complete < values
+    //  && complete < values
+    //
+    int complete = 0, values = Facuilty.getValues(), even = 0;
+
+    for (int i = 0; i < totalRooms && complete <= values; i++)
+    {
+        for (int j = 0; j < 5 && complete <= values; j++)
+        {
+            for (int k = 0; k < 8 && complete <= values; k++)
+            {
+            a:
+                string department = Facuilty.first->departmentName,
+                       subject = Facuilty.first->course.returnName(),
+                       full = department + " " + subject;
+                while (!(Facuilty.first->course.checkchange()) && complete <= values)
+                {
+                    if (Facuilty.first->next == NULL)
+                    {
+                        Facuilty.first = Facuilty.restorePtr;
+                    }
+                    Facuilty.first = Facuilty.first->next;
+                    complete++;
+                    if (subject == "!")
+                    {
+                        goto a;
+                    }
+                }
+
+                if (even % 2 == 0)
+                {
+                    if (Facuilty.first->next == NULL)
+                    {
+                        Facuilty.first = Facuilty.restorePtr;
+                    }
+                    Facuilty.first = Facuilty.first->next;
+                }
+
+                rooms[i][j][k] = full;
+                sections[i][j][k] = Facuilty.first->sections;
+                totalIncludes++, even++;
+            }
+        }
+    }
+}
+
+string day(int i)
+{
+    switch (i)
+    {
+    case 1:
+    {
+        return "Monday";
+    }
+    break;
+    case 2:
+    {
+        return "Tuesday";
+    }
+    break;
+    case 3:
+    {
+        return "wednesday";
+    }
+    break;
+    case 4:
+    {
+        return "Thrusday";
+    }
+    break;
+    case 5:
+    {
+        return "Friday";
+    }
+    break;
+    }
+    return "Hello";
+}
