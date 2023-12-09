0d843f0a70690efd75fd65ae917b04010332beae
Muhammad Asim <asimqwe2@gmail.com>
2023-12-09 22:58:13 +0500

    main function logic

---
+++ logics.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+    ClassName Faculity;
+
+    cout << "\t\t Hello World\n"
+         << "\t\t Welcome TO Time Table Program"
+         << "\n\n"
+         << "Please selsect yout option: \n"
+         << "Press 1 to generate time table\n"
+         << "Press 2 to add items to DataBase\n"
+         << "Press 3 to display items from DataBase DataBase\n";
+
+    int choice;
